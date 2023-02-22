eb9d3845deda50a64b12b7c13f05a887e571ca92
hossygifty <hossy.monday1997@gmail.com>
2023-02-22 06:41:14 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - the function of last digit
+ *
+ * @f: parameter function of checks
+ *
+ * Return: always k
+ */
+
+int print_last_digit(int f)
+{
+	int k;
+
+	k = f % 10;
+	if (f < 0)
+		k = -k;
+	return (k);
+}
+
+
