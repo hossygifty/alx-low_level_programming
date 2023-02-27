bdb3330ad06ab8a154026968ac0adf33751c1df3
hossygifty <hossy.monday1997@gmail.com>
2023-02-27 10:05:35 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,13 +8,28 @@
 
 int main(void)
 {
-	int i;
-
-	for (i = 0; i < 100 ; i++)
-	{
-	putchar((i / 10) + '0');
-	putchar((i % 10) + '0');
-	putchar('\n');
-	}
-	return (0);
+int i;
+int j;
+int n = 100;
+for (i = 0; i < n - 1; i++)
+{
+for (j = i + 1; j < n; j++)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if (i + j != 197)
+{
+putchar(',');
+putchar(' ');
+}
+else 
+{
+putchar(' ');
+}
+}
+}
+return (0);
 }
