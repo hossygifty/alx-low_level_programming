e6342c17969a8a4dff5443dc386a0f34718a3e2c
hossygifty <hossy.monday1997@gmail.com>
2023-03-04 03:11:45 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int a;
+	int y;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (y = 0; y < 52; y++)
+		{
+			if (s[a] == data1[y])
+			{
+				s[a] = datarot[y];
+				break;
+			}
+		}
+	}
+	return (s);
+}
