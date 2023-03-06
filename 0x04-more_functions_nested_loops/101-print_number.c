4818794af97c07d732b52595338934adb3c7188e
hossygifty <hossy.monday1997@gmail.com>
2023-03-05 16:54:06 -0800

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,47 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_number - function that prints an integer.
- * Return: always 0
+ * prime_factors - function declaration
+ * Description: finds largest prime factor
+ * @test_num: number to test
+ * Return: void
  */
 
-void print_number(int n)
-{
-	print_number(98);
-	_putchar('\n');
-	print_number(402);
-	_putchar('\n');
-	print_number(1024);
-	_putchar('\n');
-	print_number(0);
-	_putchar('\n');
-	print_number(-98);
-	_putchar('\n');
+void prime_factors(long test_num);
 
+/**
+ * main - entry point
+ * Description: calls prime_factors
+ * Return: 0
+ */
+
+int main(void)
+{
+	prime_factors(612852475143);
 	return (0);
+}
+
+/**
+ * prime_factors - function definition
+ * Description: finds the largest prime factor
+ * @test_num: the number to test
+ * Return: void
+ */
+
+void prime_factors(long test_num)
+{
+	long divisor = 2;
+
+	while (divisor != test_num)
+	{
+		if (test_num % divisor == 0)
+		{
+			test_num /= divisor;
+		}
+		else
+		{
+			divisor++;
+		}
+	}
+	printf("%ld\n", test_num);
 }
