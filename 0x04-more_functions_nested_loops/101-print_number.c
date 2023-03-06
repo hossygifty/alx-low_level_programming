80dbc29dbcbf757499ed2d65896a35da72b3e655
hossygifty <hossy.monday1997@gmail.com>
2023-03-05 16:23:02 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer.
+ * Return: always 0
+ */
+
+void print_number(int n)
+{
+	print_number(98);
+	_putchar('\n');
+	print_number(402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+
+	return (0);
+}
